3d64438a20ce69377f52c07c4ba93e7aff67a5f2
Jithin-caz <117753804+Jithin-caz@users.noreply.github.com>
2023-06-08 05:30:12 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Nav from './components/Navbar';
 import Tuts from './components/tutorials';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
-import About from './components/about';
+import {BrowserRouter as Router,Routes,Route,} from 'react-router-dom';
 import Login from './components/login';
+import About from './components/about';
 function App() {
   return (
 <Router>
 <Nav/>
 <Routes>
+<Route path='/' element={<Tuts/>}/>
+<Route path='/components/about' element={<About/>}/>
 <Route path='/' element={<Tuts/>}/>
 <Route path='/components/login' element={<Login/>}/>
 </Routes>
